4282e45340a55e12bb2bf9725eb9e60e549e37e7
faizanali22 <muhammadfaizan.wdc@Outlook.com>
2023-06-21 01:25:47 +0500

    Refeactor API into contact controller

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,26 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+    getContacts,
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact,
+  } = require("../controllers/contactController");
 
-router.route("/").get((req,res)=>{
-    res.json({message: "Get all contacts"});
-});
+router.route("/").get(getContacts).post(createContact)
     
-router.route("/:id").get((req,res)=>{
-    res.json({message: `Get contact for ${req.params.id}` });
-});
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
- router.route("/:id").put((req,res)=>{
-    res.json({message: `Update contact for ${req.params.id}`});
- });
-
-router.route("/:id").delete((req,res)=>{
-    res.json({message: `Delete contact for ${req.params.id}`});
-});
-
- router.route("/").post((req,res)=>{
-    res.json({message: "Create Contact."});
- });
-
-
+ //router.route("/:id")
+//router.route("/:id")
+ //router.route("/")
 module.exports = router;
