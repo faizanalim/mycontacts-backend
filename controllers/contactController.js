898ab841d3d58ef320b4e47af2699a57a5249725
faizanali22 <muhammadfaizan.wdc@Outlook.com>
2023-06-22 12:14:23 +0500

    complete crud

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,33 +1,82 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel.js");
 
 const getContacts = asyncHandler(async(req,res)=>
 {
-    res.status(200).json({message: "Get all contacts"});
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
     });
 
-const createContact = asyncHandler(async(req,res)=>{
-    console.log("The request body : ", req.body);
-    const{name,email,phone} = req.body;
-    if(!name || !email || !phone)
-    {
-        res.status(400);
-        throw new Error("All fields are manadtory !")
-    }
-
-    res.status(201).json({message: "Create Contact."});
- });
-
- const getContact = asyncHandler(async(req,res)=>{
-    res.status(201).json({message: `Get contact for ${req.params.id}` });
+    const createContact = asyncHandler(async (req, res) => {
+        
+        const { name, email, phone } = req.body;
+       
+        if (!name || !email || !phone) {
+          res.status(400);
+          throw new Error("All fields are mandatory !");
+        }
+        const contact = await Contact.create({
+          name,
+          email,
+          phone,
+         // user_id: req.user.id,
         });
+        console.log(contact);
+        res.status(201).json(contact);
+      });
 
-const updateContact = asyncHandler(async (req,res)=>{
-    res.status(200).json({message: `Update contact for ${req.params.id}`});
-            });
+//@desc Get contact
+//@route GET /api/contacts/:id
+//@access private
+const getContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
+    res.status(200).json(contact);
+  });
 
-            const deleteContact = asyncHandler(async (req,res)=>{
-                res.status(201).json({message: `Delete contact for ${req.params.id}`});
-                            });
+//@desc Update contact
+//@route PUT /api/contacts/:id
+//@access private
+const updateContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
+  
+   /*  if (contact.user_id.toString() !== req.user.id) {
+      res.status(403);
+      throw new Error("User don't have permission to update other user contacts");
+    } */
+  
+    const updatedContact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+  
+    res.status(200).json(updatedContact);
+  });
+
+//@desc Delete contact
+//@route DELETE /api/contacts/:id
+//@access private
+const deleteContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
+    /* if (contact.user_id.toString() !== req.user.id) {
+      res.status(403);
+      throw new Error("User don't have permission to update other user contacts");
+    } */
+    await Contact.deleteOne({ _id: req.params.id });
+    res.status(200).json(contact);
+  });
 
                             module.exports = {
                                 getContacts,
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,7 @@
 const {constants} =require("../constants")
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
-    switch (satusCode) {
+    switch (statusCode) {
         case constants.VALIDATION_ERROR:
             res.json({tittle:"Validation Failed!",
             message:err.message, stackTrace: err.stack});
